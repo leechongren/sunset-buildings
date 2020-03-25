c3595fb923e891eead29d576f35e975092b97131
leechongren <lee_chongren@hotmail.com>
2020-03-25 14:13:42 +0800

    Initial set up

---
+++ sunset-buildings.js
@@ -0,0 +1,5 @@
+const sunsetBuildings = arrOfBuildings => {
+  return 1;
+};
+
+module.exports = sunsetBuildings;
---
+++ sunset-buildings.test.js
@@ -0,0 +1,7 @@
+const sunsetBuildings = require("./sunset-buildings");
+
+describe("Sunset Buildings", () => {
+  it("should return 1", () => {
+    expect(sunsetBuildings()).toBe(1);
+  });
+});
